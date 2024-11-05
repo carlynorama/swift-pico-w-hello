b6bded1b994527ac40d5ca1f0dfd3ddd21f7d1cb
carlynorama <carlynorama@gmail.com>
2024-11-05 08:58:06 -0800

    badge i2c 1,2,3

--- helper.h
+++ helper.h
@@ -5,6 +5,7 @@ int addNumbers(int a, int b);
 
 int i2c_setup_default(void);
 int i2c_default_address_check(int addr);
-int i2c_setup_i2c0(int SDA_PIN, int SCL_PIN);
+int i2c_setup_i2c0(int SDA_PIN, int SCL_PIN, int BAUD_RATE);
+int i2c_i2c0_address_check(int addr);
 
 #endif

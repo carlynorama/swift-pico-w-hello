5357093593cdabe3567af9a0a8a3392035912600
carlynorama <carlynorama@gmail.com>
2024-11-11 10:11:55 -0800

    working USB support

--- 08-StrippedDownBlink/Main.swift
+++ 08-StrippedDownBlink/Main.swift
@@ -54,38 +54,11 @@ struct USBSerial {
         let _ = usb_init_hardware()
     }
 
-    static func send(_ c:UnsafePointer<CChar>) {
-        usb_serial_send(c)
+    static func send(_ message:StaticString) {
+        //usb_serial_send(c)
+        message.withUTF8Buffer { bufferPointer in
+            usb_serial_send(bufferPointer.baseAddress!)
+        }
     }
 }
 
-
-/// Implement `posix_memalign(3)`, which is required by the Swift runtime but is
-/// not provided by the pico-sdk library which does not support C-99
-//from search for posix_memalign using language Swift
-// https://github.com/apple/swift-playdate-examples/blob/749dd8f518429168d03e754764afb334a80b527d/Sources/Playdate/Playdate.swift#L21
-@_documentation(visibility: internal)
-@_cdecl("posix_memalign")
-public func posix_memalign(
-  _ memptr: UnsafeMutablePointer<UnsafeMutableRawPointer?>,
-  _ alignment: Int,
-  _ size: Int
-) -> CInt {
-  guard let allocation = malloc(Int(size + alignment - 1)) else {
-    #if hasFeature(Embedded)
-    fatalError()
-    #else
-    fatalError("Unable to handle memory request: Out of memory.")
-    #endif
-  }
-  let misalignment = Int(bitPattern: allocation) % alignment
-  #if hasFeature(Embedded)
-  precondition(misalignment == 0)
-  #else
-  precondition(
-    misalignment == 0,
-    "Unable to handle requests for over-aligned memory.")
-  #endif
-  memptr.pointee = allocation
-  return 0
-}
